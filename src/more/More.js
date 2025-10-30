07d9d574b5f86f8ef9ca7d2168d49ded9ad10bbd
rambod <rambodc@irobotx.io>
2025-10-30 12:18:29 -0600

    updated the more page and moved many pages to archive folder

--- src/more/More.js
+++ src/more/More.js
@@ -11,13 +11,11 @@ import {
   FaFileAlt,
   FaCogs,
   FaPaintBrush,
-  FaCheckCircle,
   FaSignOutAlt,
   FaChevronRight,
   FaPlus,
   FaUserPlus,
   FaUserCog,
-  FaWallet,
 } from 'react-icons/fa';
 
 export default function More() {
@@ -55,10 +53,8 @@ export default function More() {
           <div className={styles.list}>
             <Item icon={FaUserCog} color="#111827" label="Account" onClick={() => navigate('/account')} />
             <Item icon={FaPaintBrush} color="#ec4899" label="Become an Artist" onClick={() => navigate('/chat')} />
-            <Item icon={FaCheckCircle} color="#10b981" label="Validate Drops" onClick={() => navigate('/validate-drops')} />
             <Item icon={FaFileAlt} color="#0ea5e9" label="Terms" onClick={() => navigate('/terms')} />
             <Item icon={FaCogs} color="#6366f1" label="Services" onClick={() => navigate('/services')} />
-            <Item icon={FaWallet} color="#0ea5e9" label="Balance" onClick={() => navigate('/balance')} />
             <Item icon={FaUserPlus} color="#2563eb" label="Create Artist" onClick={() => navigate('/create-artists')} />
             <Item icon={FaPlus} color="#111827" label="Create Drop" onClick={() => navigate('/create-drop')} />
             <Item icon={FaSignOutAlt} color="#ef4444" label="Logout" onClick={onLogout} className={styles.itemSpacing} />
