e0230480b3472b1a77c0459b56e56978d9f604c6
rambod <rambodc@irobotx.io>
2025-12-16 14:40:09 -0700

    Revert "updated stripe cards"
    This reverts commit 51c118e8d80be107bb7f31fe608e8be63c6fffcb.

--- functions/index.js
+++ functions/index.js
@@ -6,9 +6,6 @@ import { ensureStripeCustomer } from './ensureStripeCustomer.js';
 import { stripeWebhook } from './stripeWebhook.js';
 import { listPaymentMethods } from './listPaymentMethods.js';
 import { createCustomerPortalSession } from './createCustomerPortalSession.js';
-import { createSetupIntent } from './createSetupIntent.js';
-import { setDefaultPaymentMethod } from './setDefaultPaymentMethod.js';
-import { deletePaymentMethod } from './deletePaymentMethod.js';
 
 export {
   createStripeCheckoutSession,
@@ -16,7 +13,4 @@ export {
   stripeWebhook,
   listPaymentMethods,
   createCustomerPortalSession,
-  createSetupIntent,
-  setDefaultPaymentMethod,
-  deletePaymentMethod,
 };
--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Elements } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
 import './index.css';
 import App from './App';
-
-const publishableKey = process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY || '';
-const stripePromise = publishableKey ? loadStripe(publishableKey) : null;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {stripePromise ? (
-      <Elements stripe={stripePromise}>
-        <App />
-      </Elements>
-    ) : (
-      <App />
-    )}
+    <App />
   </React.StrictMode>
 );
