bfeb3f2958646f39deb155409b9b916085674466
rambod <rambodc@irobotx.io>
2026-02-15 10:46:00 -0700

    Logo update

--- src/home/Home.js
+++ src/home/Home.js
@@ -167,7 +167,7 @@ export default function Home() {
 
       <header className="home-topbar">
         <div className="home-brand">
-          <img src={`${process.env.PUBLIC_URL}/assets/quotechem-logo.png`} alt="QuoteChem" className="home-logo" />
+          <img src={`${process.env.PUBLIC_URL}/assets/QuoteChem Logo2.png`} alt="QuoteChem" className="home-logo" />
         </div>
       </header>
 
